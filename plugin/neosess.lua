759befd8f56e985d12a196855a230777347f001d
ksaito <kitsit.39@gmail.com>
2023-07-17 16:54:41 +0900

    fix

--- plugin/neosess.lua
+++ plugin/neosess.lua
@@ -4,5 +4,4 @@ end
 vim.g.loaded_neosess = 1
 
 vim.api.nvim_command('command! -nargs=1 SaveSession lua require("neosess").save_session(<f-args>)')
-vim.api.nvim_command('command! -nargs=1 LoadSession lua require("neosess").load_session(<f-args>)')
-vim.api.nvim_command('command! -nargs=0 DisplaySession lua require("neosess").display_session_files()')
+vim.api.nvim_command('command! -nargs=0 LoadSession lua require("neosess").display_session_files()')
