a71d48319d1f1a6302addbf798cadfb4a6f3de03
ksaito <kitsit.39@gmail.com>
2023-07-18 08:36:01 +0900

    format: stylua

--- lua/neosess/init.lua
+++ lua/neosess/init.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(opts)
     -- Define default values
     -- TODO: localで定義したい
-    session_path = opts.session_path or "~/.config/nvim/sessions"
+    session_path = opts.session_path or '~/.config/nvim/sessions'
     -- TODO: 空文字列, nilなどの不正な値ならエラーにしたい
     session_path = vim.fn.expand(session_path)
 end
@@ -12,7 +12,7 @@ end
 local function dir_exists()
     local dir_exists = vim.loop.fs_stat(session_path)
     if not dir_exists then
-        os.execute("mkdir -p " .. session_path)
+        os.execute('mkdir -p ' .. session_path)
     end
 end
 
@@ -20,32 +20,32 @@ end
 local function file_exists(session_file_path)
     local file_exists = vim.loop.fs_stat(session_file_path)
     if not file_exists then
-        local f = assert(io.open(session_file_path, "w"))
+        local f = assert(io.open(session_file_path, 'w'))
         f:close()
     end
 end
 
 -- セッションを保存する
 function M.save_session(file)
-    local session_file_path = session_path .. "/" .. file .. ".vim"
+    local session_file_path = session_path .. '/' .. file .. '.vim'
 
     dir_exists()
     file_exists(session_file_path)
 
-    vim.api.nvim_command("mksession! " .. session_file_path)
-    vim.api.nvim_command("redraw")
-    print("neosess: session created")
+    vim.api.nvim_command('mksession! ' .. session_file_path)
+    vim.api.nvim_command('redraw')
+    print('neosess: session created')
 end
 
 -- 保存したセッションファイル一覧を取得する
 local function fetch_session_file()
     local function readdir()
-        return vim.fn.globpath(session_path, "*", 1, 1)
+        return vim.fn.globpath(session_path, '*', 1, 1)
     end
 
     local result = {}
     for _, file in ipairs(readdir(session_path)) do
-        if vim.fn.isdirectory(session_path .. "/" .. file) == 0 then
+        if vim.fn.isdirectory(session_path .. '/' .. file) == 0 then
             table.insert(result, file)
         end
     end
@@ -89,14 +89,20 @@ local function show_table_in_float_win(t)
     end
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
     -- TODO: local function process_lineを呼び出したい
-    vim.api.nvim_buf_set_keymap(buf, "n", "<CR>", string.format(":lua process_line(vim.api.nvim_get_current_line(), %d)<CR>", win_id), {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(
+        buf,
+        'n',
+        '<CR>',
+        string.format(':lua process_line(vim.api.nvim_get_current_line(), %d)<CR>', win_id),
+        { noremap = true, silent = true }
+    )
     -- vim.api.nvim_buf_set_current_win(win_id)
 end
 
 -- 保存したセッションを読み込む
 function load_session(file, win_id)
     vim.api.nvim_win_close(win_id, true)
-    vim.api.nvim_command("source " .. file)
+    vim.api.nvim_command('source ' .. file)
 end
 
 -- session fileを一覧表示する
