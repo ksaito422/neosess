4fb80ed1b0da83cb50dc5a8ce45e24d10172a01f
ksaito <kitsit.39@gmail.com>
2023-09-04 20:10:12 +0900

    fix: command name

--- plugin/neosess.lua
+++ plugin/neosess.lua
@@ -3,5 +3,5 @@ if vim.g.loaded_neosess then
 end
 vim.g.loaded_neosess = 1
 
-vim.api.nvim_command('command! -nargs=1 SaveSession lua require("neosess").save_session(<f-args>)')
-vim.api.nvim_command('command! -nargs=0 LoadSession lua require("neosess").display_session_files()')
+vim.api.nvim_command('command! -nargs=1 NeosessSave lua require("neosess").save_session(<f-args>)')
+vim.api.nvim_command('command! -nargs=0 NeosessList lua require("neosess").display_session_files()')
