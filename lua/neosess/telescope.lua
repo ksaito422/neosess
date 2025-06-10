9ec448f1c379a88e4d39b5ac013801f9b946501f
ksaito <kitsit.39@gmail.com>
2025-06-10 20:01:53 +0900

    feat: display the datetime the session was saved

--- lua/neosess/telescope.lua
+++ lua/neosess/telescope.lua
@@ -21,10 +21,24 @@ M.sessions = function(opts)
             return vim.fn.globpath(SessionPath, '*', 1, 1)
         end
 
+        local function format_datetime(timestamp)
+            return os.date('%Y-%m-%d %H:%M:%S', timestamp)
+        end
+
         local session_files = {}
         for _, file in ipairs(readdir(SessionPath)) do
             if vim.fn.isdirectory(SessionPath .. '/' .. file) == 0 then
-                table.insert(session_files, file)
+                local filename = vim.fn.fnamemodify(file, ':t')
+                local stat = vim.loop.fs_stat(file)
+                local datetime = stat and format_datetime(stat.mtime.sec) or 'Unknown'
+                local display = filename .. ' (' .. datetime .. ')'
+                
+                table.insert(session_files, {
+                    display = display,
+                    filename = filename,
+                    path = file,
+                    datetime = datetime
+                })
             end
         end
 
@@ -37,6 +51,14 @@ M.sessions = function(opts)
             -- telescopeに表示する値
             finder = finders.new_table({
                 results = fetch_session_files(),
+                entry_maker = function(entry)
+                    return {
+                        value = entry,
+                        display = entry.display,
+                        ordinal = entry.filename .. ' ' .. entry.datetime,
+                        path = entry.path
+                    }
+                end
             }),
             sorter = conf.generic_sorter(opts),
             attach_mappings = function(prompt_bufnr, map)
@@ -44,18 +66,18 @@ M.sessions = function(opts)
                 actions.select_default:replace(function()
                     actions.close(prompt_bufnr)
                     local selection = action_state.get_selected_entry()
-                    vim.api.nvim_command('source ' .. selection[1])
+                    vim.api.nvim_command('source ' .. selection.path)
                 end)
 
                 -- <ctrl-;>でセッションファイルを削除する
                 local delete_session = function(prompt_bufnr)
                     local selection = action_state.get_selected_entry()
-                    local filename = vim.fn.fnamemodify(selection[1], ':t')
+                    local filename = selection.value.filename
 
                     -- 削除確認
                     local confirm = vim.fn.confirm('Delete session "' .. filename .. '"?', '&Yes\n&No', 2)
                     if confirm == 1 then
-                        os.remove(selection[1])
+                        os.remove(selection.path)
                         vim.api.nvim_echo({ { 'neosess: ' .. filename .. ' has been deleted.' } }, true, {})
 
                         -- 画面リフレッシュ
