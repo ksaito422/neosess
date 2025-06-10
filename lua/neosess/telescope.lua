bddcf5cf93b3cff58bb496dcf9e29028cdb450aa
ksaito <kitsit.39@gmail.com>
2025-06-10 19:48:07 +0900

    chore: stylua

--- lua/neosess/telescope.lua
+++ lua/neosess/telescope.lua
@@ -31,43 +31,45 @@ M.sessions = function(opts)
         return session_files
     end
 
-    pickers.new(opts, {
-        prompt_title = 'sessions',
-        -- telescopeに表示する値
-        finder = finders.new_table {
-            results = fetch_session_files()
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
-            -- <Enter>でセッションに保存してあるファイルを開く
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                vim.api.nvim_command('source ' .. selection[1])
-            end)
-
-            -- <ctrl-;>でセッションファイルを削除する
-            local delete_session = function(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                local filename = vim.fn.fnamemodify(selection[1], ':t')
-
-                -- 削除確認
-                local confirm = vim.fn.confirm('Delete session "' .. filename .. '"?', '&Yes\n&No', 2)
-                if confirm == 1 then
-                    os.remove(selection[1])
-                    vim.api.nvim_echo({ { 'neosess: ' .. filename .. ' has been deleted.' } }, true, {})
-
-                    -- 画面リフレッシュ
+    pickers
+        .new(opts, {
+            prompt_title = 'sessions',
+            -- telescopeに表示する値
+            finder = finders.new_table({
+                results = fetch_session_files(),
+            }),
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(prompt_bufnr, map)
+                -- <Enter>でセッションに保存してあるファイルを開く
+                actions.select_default:replace(function()
                     actions.close(prompt_bufnr)
-                    M.sessions(opts)
+                    local selection = action_state.get_selected_entry()
+                    vim.api.nvim_command('source ' .. selection[1])
+                end)
+
+                -- <ctrl-;>でセッションファイルを削除する
+                local delete_session = function(prompt_bufnr)
+                    local selection = action_state.get_selected_entry()
+                    local filename = vim.fn.fnamemodify(selection[1], ':t')
+
+                    -- 削除確認
+                    local confirm = vim.fn.confirm('Delete session "' .. filename .. '"?', '&Yes\n&No', 2)
+                    if confirm == 1 then
+                        os.remove(selection[1])
+                        vim.api.nvim_echo({ { 'neosess: ' .. filename .. ' has been deleted.' } }, true, {})
+
+                        -- 画面リフレッシュ
+                        actions.close(prompt_bufnr)
+                        M.sessions(opts)
+                    end
                 end
-            end
 
-            map('n', '<C-;>', delete_session)
+                map('n', '<C-;>', delete_session)
 
-            return true
-        end,
-    }):find()
+                return true
+            end,
+        })
+        :find()
 end
 
 return M
--- plugin/neosess.lua
+++ plugin/neosess.lua
@@ -4,4 +4,7 @@ end
 vim.g.loaded_neosess = 1
 
 vim.api.nvim_command('command! -nargs=1 NeosessSave lua require("neosess").save_session(<f-args>)')
-vim.api.nvim_command('command! -nargs=0 NeosessList lua require("neosess").display_session_files(require("telescope.themes").get_dropdown {})')
+vim.api.nvim_command(
+    'command! -nargs=0 NeosessList lua require("neosess").display_session_files(require("telescope.themes").get_dropdown {})'
+)
+
